f4637a0f71f444c6ad8fdff38e411dd2350f44dc
hrushiraj <hrushirajg23@gmail.com>
2024-12-30 20:11:04 +0530

    tired

--- .high_mem.mod.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/hrushiraj/Desktop/black/high_mem.mod := printf '%s\n'   high_mem.o | awk '!x[$$0]++ { print("/home/hrushiraj/Desktop/black/"$$0) }' > /home/hrushiraj/Desktop/black/high_mem.mod
--- .modules.order.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/hrushiraj/Desktop/black/modules.order := {   echo /home/hrushiraj/Desktop/black/high_mem.o; :; } > /home/hrushiraj/Desktop/black/modules.order
